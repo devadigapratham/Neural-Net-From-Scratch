6228267bdf83fb5beaebc4f6de0e62d8029e47b4
Prathamesh Devadiga <71978538+devadigapratham@users.noreply.github.com>
2023-09-29 12:41:06 +0530

    Changed from 2 dense layers to User input

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -7,6 +7,9 @@ from evaluate_model import evaluate_model
 # Load the synthetic spiral data
 X, y = load_data('spiral_data.pkl')
 
+# Get user input for the number of dense layers
+num_dense_layers = int(input("Enter the number of dense layers: "))
+
 # Define hyperparameter grid
 param_grid = {
     'num_neurons': [3, 4, 5],
@@ -18,39 +21,50 @@ best_params = None
 
 # Perform grid search
 for num_neurons, learning_rate in product(param_grid['num_neurons'], param_grid['learning_rate']):
-    # Create and train the neural network model
-    dense1 = Layer_Dense(2, num_neurons)
-    activation1 = Activation_ReLU()
-    dense2 = Layer_Dense(num_neurons, 3)
-    activation2 = Activation_SoftMax()
+    # Create dense layers based on user input
+    dense_layers = [Layer_Dense(2, num_neurons) for _ in range(num_dense_layers)]
+    activation_layers = [Activation_ReLU() for _ in range(num_dense_layers)]
+    
+    output_layer = Layer_Dense(num_neurons, 3)
+    softmax_activation = Activation_SoftMax()
     loss_function = Loss_CategoricalCrossEntropy()
     optimizer = Optimizer_SGD(learning_rate=learning_rate)
 
     for epoch in range(10001):
         # Forward pass
-        dense1.forward(X)
-        activation1.forward(dense1.output)
-        dense2.forward(activation1.output)
-        activation2.forward(dense2.output)
+        layer_output = X
+        for i in range(num_dense_layers):
+            dense_layer = dense_layers[i]
+            activation_layer = activation_layers[i]
+            dense_layer.forward(layer_output)
+            activation_layer.forward(dense_layer.output)
+            layer_output = activation_layer.output
+        
+        output_layer.forward(layer_output)
+        softmax_activation.forward(output_layer.output)
 
         # Calculate loss
-        loss = loss_function.forward(activation2.output, y)
+        loss = loss_function.calculate(softmax_activation.output, y)
 
         # Backward pass
-        loss_function.backward(activation2.output, y)
-        dense2.backward(loss_function.dvalues)
-        activation1.backward(dense2.dvalues)
-        dense1.backward(activation1.dvalues)
+        loss_function.backward(softmax_activation.output, y)
+        output_layer.backward(loss_function.dvalues)
+        
+        for i in range(num_dense_layers - 1, -1, -1):
+            activation_layer = activation_layers[i]
+            dense_layer = dense_layers[i]
+            activation_layer.backward(dense_layer.dvalues)
+            dense_layer.backward(activation_layer.dvalues)
 
         # Update weights and biases
-        optimizer.update_params(dense1)
-        optimizer.update_params(dense2)
+        for i in range(num_dense_layers):
+            optimizer.update_params(dense_layers[i])
 
         if epoch % 1000 == 0:
             print(f"Epoch: {epoch}, Loss: {loss:.4f}")
 
     # Evaluate the model on validation data
-    accuracy = evaluate_model((dense1, activation1, dense2, activation2), X, y)
+    accuracy = evaluate_model((*dense_layers, output_layer, softmax_activation), X, y)
     print(f"Hyperparameters: Neurons={num_neurons}, Learning Rate={learning_rate}")
     print(f"Validation Accuracy: {accuracy:.4f}")
     print()
